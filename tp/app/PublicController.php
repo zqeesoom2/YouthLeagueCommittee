1ed2b429e90c3fa1b566adcca42bf80c973ced19
zqeesoom2 <zqeesoom@126.com>
2020-05-07 15:19:20 +0800

    uc双向登陆0.1

--- tp/app/PublicController.php
+++ tp/app/PublicController.php
@@ -1,18 +1,21 @@
 <?php
 namespace app;
 
+use app\ucenter\UcApi;
+use think\facade\Cookie;
 
 trait PublicController{
 
-    public function _initialize() {
+    public function __construct() {
         $this->autologin();
     }
 
     public function autologin() {
-        if (isset($_COOKIE['aY4z_2132_auth']) && !empty($_COOKIE['aY4z_2132_auth'])) {
-            Vendor('ThinkphpUcenter.UcApi');//载入UcApi扩展
-            $key=md5(C('AUTH_KEY').$_COOKIE['aY4z_2132_saltkey']);//获取在本应用的配置文件config.php中的解密钥匙		
-            $userMsg = explode("\t", uc_authcode($_COOKIE['aY4z_2132_auth'], 'DECODE', $key)); //得到加了密的password【$userMsg[0]】和uid【$userMsg[1]】
+        halt(Cookie::get());
+        if (Cookie::has('vszu_dc7c_auth')) {
+            //discuz $_config['security']['authkey'] = 'ce1f2cLQg1D6J6xn';
+            $key=md5('ce1f2cLQg1D6J6xn'.Cookie::get('vszu_dc7c_saltkey'));//获取在本应用的配置文件config.php中的解密钥匙
+            $userMsg = explode("\t", uc_authcode(Cookie::get('vszu_dc7c_auth'), 'DECODE', $key)); //得到加了密的password【$userMsg[0]】和uid【$userMsg[1]】
             $userInfo = uc_get_user($userMsg[1], 1);//通过uid获取username
 			$_SESSION['user_id'] = $userMsg[1];
             $_SESSION['username'] = $userInfo[1];
